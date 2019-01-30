6c9c39f5dd314ccae5b58bd1876d7e0531c23551
Mateus Felix <mateus.cfoliveira@gmail.com>
2019-01-30 09:42:59 -0200

    Solved

---
+++ javascript/simple-transposition.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/simple-transposition
+
+const simpleTransposition = text => {
+  let first = '';
+  let second = '';
+  for (let i = 0; i < text.length; i += 1) {
+    if (i % 2 === 0) {
+      first += text[i];
+    } else {
+      second += text[i];
+    }
+  }
+  return `${first}${second}`;
+};
